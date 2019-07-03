8fd5e53a405a959d50e2d25fa7bd100e64abac9e
asingh <singajeet@gmail.com>
2019-07-03 19:48:45 +0530

    Added Accordion section

--- widgets4py/jquery_ui.py
+++ widgets4py/jquery_ui.py
@@ -5,48 +5,45 @@ Date: 06/25/2019
 from widgets4py.base import Widget
 
 
-class JQButton(Widget):
+class Section(Widget):
     """JQuery Button class"""
 
     _title = None
     _app = None
     _onclick_callback = None
+    _disabled = None
+    _required = None
 
-    def __init__(self, name, title, app=None, onclick_callback=None):
-        """default constructor"""
-        Widget.__init__(self, name)
+    def __init__(self, name, title, for_widget, desc=None, prop=None, style=None, attr=None,
+                 disabled=False, required=False, onclick_callback=None, app=None, css_cls=None):
+        """Default constructor of the Label widget class
+
+            Args:
+                name (string): name of the widget for internal use
+                title (string): title of the button widget
+                desc (string, optional): description of the button widget
+                prop (dict, optional): dict of objects to be added as properties of widget
+                style (dict, optional): dict of objects to be added as style elements to HTML tag
+                attr (list, optional): list of objects to be added as attributes of HTML tag
+                disabled (Boolean, optional): Enabled or Disabled state of widget
+                required (Boolean, optional): Widget is required to be filled-in or not
+                onclick_callback (function, optional): A function to be called back on onclick event
+                app (Flask, optional): An instance of Flask class
+                css_cls (list, optional): An list of CSS class names to be added to current widget
+        """
+        Widget.__init__(self, name, desc=desc, prop=prop, style=style, attr=attr,
+                        css_cls=css_cls)
         self._title = title
         self._app = app
         self._onclick_callback = onclick_callback
+        self._disabled = disabled
+        self._required = required
 
     def render(self):
-        """Renders the JQuery button on the page"""
-        url = ''
-        content = ''
-        if self._app is not None and self._onclick_callback is not None:
-            url = str(__name__ + "_" + self._name).replace('_', '.')
-            content = """<button id='%s' name='%s'>%s</button>
-                        <script>
-                            $(function(){
-                                $('#%s').button();
-                                $('#%s').click(function(event){
-                                    $.ajax({
-                                        url: '/%s',
-                                        success: function(status){alert('success');},
-                                        error: function(status){alert('error');}
-                                    });
-                                });
-                            });
-                        </script>
-                    """ % (self._name, self._name, self._title, self._name, self._name, url)
-            self._app.add_url_rule('/' + url, url, self._onclick_callback)
-        else:
-            content = """<button id='%s' name='%s' value='%s'></button>
-                        <script>
-                            $(function(){
-                                $('%s').button();
-                            });
-                        </script>
-                    """ % (self._name, self._name, self._title, self._name)
+        content = "<h3>" + self._title + "</h3>\n"
+        content += self._render_pre_content('div')
+        for widget in self._child_widgets:
+            content += "\n" + widget.render()
+        content += self._render_post_content('div')
         self._widget_content = content
         return self._widget_content
